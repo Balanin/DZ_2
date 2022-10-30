56d1a09550a7e9939ce971dff8fdf57e7f2afc4a
Balanin <69090346+Balanin@users.noreply.github.com>
2022-10-30 19:09:44 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(name='clean_folder',
+      version='1.2',
+      packages = find_packages(), 
+      description='Very useful code',
+      long_description=open('README.md').read(),
+      author='Volodymyr Balan',
+      author_email='vova.balan1988@gmail.com',
+      #license='MIT',
+      )
